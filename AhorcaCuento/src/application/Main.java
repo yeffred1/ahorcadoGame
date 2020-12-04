076dfb6005395cd7e6a91d0dcddde2511f6f0cc7
Desarrollo <desarrollo@gwspirits.com>
2020-12-04 11:52:54 -0500

    20201204
    creando interfaz Grafica

--- AhorcaCuento/src/application/Main.java
+++ AhorcaCuento/src/application/Main.java
@@ -1,21 +1,24 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//ScrollPane root = (ScrollPane) FXMLLoader.load(getClass().getResource("/edu/upoli/fx1/view/Form.fxml"));
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/aplication/escenarios/inicio.fxml"));
+			Scene scene = new Scene(root, 405, 700);
+			scene.getStylesheets().add(getClass().getResource("/aplication/estilos/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
---
+++ AhorcaCuento/src/application/inicio.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+public class inicio {
+
+    
+}
